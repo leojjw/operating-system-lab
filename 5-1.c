b6ad1528c7503b8139954ea055c933c2af03e9d8
leojjw <leojjw@naver.com>
2022-09-26 22:13:00 -0700

    home

---
+++ 5-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main()
+{
+    pid_t pid;
+    int x = 100;
+
+    pid = fork();
+    if (pid == 0) {
+        printf("child : x=%d\n", ++x);
+        exit(0);
+    }
+
+    printf("parent: x=%d\n", --x);
+    exit(0);
+}
